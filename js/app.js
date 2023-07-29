f35721c239451efc0cb28f59833fd87ef86c7a2c
Mate Toth <tothmat@outlook.hu>
2023-07-29 18:47:16 +0200

    WWorking with variables

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
-let a = 1; 
-let b = 2; 
-let c = 5;
+let x = 1; 
+let y = 2; 
+let result = 5;
+
+// result = x + y
+
+// result *= 2;
+// result /= 4;
+// result -= 2;
+
+
+// console.log(result);
+
+
+// let isAlive = ture;
+
+// isAlive = !isAlive;
+
+// console.log(isAlive);
+
+// let bithday = new Date(1990, 8, 15, 0, 0, 0, 0)
+
+// console.log(bithday.getMonth() + 2);
+
+
+const firstName = 'Mate';
+const lastName = 'Toth';
+
+console.log(`Hello ${firstName} ${lastName} vmy name is Bob`);
+
